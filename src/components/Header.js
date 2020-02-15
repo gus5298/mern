345ac3afd499a70c0311bad57ab716daa25606b0
Gustavo <b6025590@9310-03J522003>
2020-02-15 19:12:54 +0000

    clean up code

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,8 @@ export default function Header() {
     return (
         <div style={header}>
             <h1>TMS Cloud</h1>
-                <Link style={link} to="/">Files</Link> |
+            <Link style={link} to="/signin"> Sign In</Link> | 
+                <Link style={link} to="/"> Files</Link> |
                 <Link style={link} to="/about"> About TMS Cloud</Link> |
                 <Link style={link} to="/create"> Add File</Link> 
                 {/* <Link style={link} to="/edit/:id">Edit File</Link> */}
